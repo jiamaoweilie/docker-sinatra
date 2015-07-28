89065238b077af8d98ee94c187e486e4dbf72ca7
Wei Jia <Wei.Jia@team.telstra.com>
2015-07-28 11:28:15 +0800

    update api

--- app.rb
+++ app.rb
@@ -13,20 +13,21 @@ ActiveRecord::Base.establish_connection(
 class User < ActiveRecord::Base
 end
 
-ActiveRecord::Migration.create_table :users do |t|
-  t.string :name
-end
-
 class App < Sinatra::Application
 end
 
-get '/' do
-  @users = User.all
-  @users.to_json
+get '/users' do
+  users = User.all
+  users.to_json
 
 end
 
-post '/' do
+post '/users' do
   user = User.create(:name => params[:name])
   user.save
+end
+
+get '/users/:id' do
+  user = User.find(params[:id])
+  user.to_json
 end
