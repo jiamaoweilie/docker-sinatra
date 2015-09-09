cf1d36481798efe644f47e497c0244bb5cdf6de8
Wei Jia <Wei.Jia@team.telstra.com>
2015-09-09 16:11:19 +0800

    origin hello world

--- app.rb
+++ app.rb
@@ -1,33 +1,7 @@
-# encoding: UTF-8
 require 'sinatra'
-require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-    :adapter => "mysql2",
-    :host => "192.168.33.10",
-    :username => "root",
-    :password => "password",
-    :database => "test"
-)
-
-class User < ActiveRecord::Base
-end
-
-class App < Sinatra::Application
-end
-
-get '/users' do
-  users = User.all
-  users.to_json
-
-end
-
-post '/users' do
-  user = User.create(:name => params[:name])
-  user.save
-end
-
-get '/users/:id' do
-  user = User.find(params[:id])
-  user.to_json
+class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
 end
